232509f626b6c8e93cffc9e20fc74a4f0bc0560b
Stewart ilondanga <stilondanga@gmail.com>
2018-06-05 14:14:50 +0300

    add app/src/main/assets/fonts/ostrich-regular.ttf

--- app/src/main/java/io/github/stewilondanga/myrestaurants/MainActivity.java
+++ app/src/main/java/io/github/stewilondanga/myrestaurants/MainActivity.java
@@ -1,23 +1,29 @@
 package io.github.stewilondanga.myrestaurants;
 
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 
 public class MainActivity extends AppCompatActivity {
     private Button mFindRestaurantsButton;
     private EditText mLocationEditText;
+    private TextView mAppNameTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        mAppNameTextView = (TextView) findViewById(R.id.textView);
+        Typeface ostrichFont = Typeface.createFromAsset(getAssets(), "fonts/ostrich-regular.ttf");
+        mAppNameTextView.setTypeface(ostrichFont);
         mLocationEditText = (EditText) findViewById(R.id.locationEditText);
         mFindRestaurantsButton = (Button) findViewById(R.id.findRestaurantsButton);
                mFindRestaurantsButton.setOnClickListener(new View.OnClickListener() {
