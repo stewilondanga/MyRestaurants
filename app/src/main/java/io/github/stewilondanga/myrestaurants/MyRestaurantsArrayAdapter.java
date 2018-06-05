2a9095cbdbaee865a62f6736545646b6e1a98404
Stewart ilondanga <stilondanga@gmail.com>
2018-06-05 17:27:25 +0300

    add app/src/main/java/io/github/stewilondanga/myrestaurants/MyRestaurantsArrayAdapter.java

---
+++ app/src/main/java/io/github/stewilondanga/myrestaurants/MyRestaurantsArrayAdapter.java
@@ -0,0 +1,22 @@
+package io.github.stewilondanga.myrestaurants;
+
+import android.content.Context;
+import android.widget.ArrayAdapter;
+
+/**
+ * Created by stewart on 6/5/18.
+ */
+
+public class MyRestaurantsArrayAdapter extends ArrayAdapter {
+    private Context mContext;
+    private String[] mRestaurants;
+    private String[] mCuisines;
+
+    public MyRestaurantsArrayAdapter(Context mContext, int resource, String[] mRestaurants, String[] mCuisines) {
+        super(mContext, resource);
+        this.mContext = mContext;
+        this.mRestaurants = mRestaurants;
+        this.mCuisines = mCuisines;
+    }
+
+}
--- app/src/main/java/io/github/stewilondanga/myrestaurants/RestaurantsActivity.java
+++ app/src/main/java/io/github/stewilondanga/myrestaurants/RestaurantsActivity.java
@@ -17,6 +17,7 @@ public class RestaurantsActivity extends AppCompatActivity {
     @BindView(R.id.listView) ListView mListView;
 
     private String[] restaurants = new String[] {"Sweet Hereafter", "Cricket", "Hawthorne Fish House", "Viking Soul Food", "Red Square", "Horse Brass", "Dick's Kitchen", "Taco Bell", "Me Kha Noodle Bar", "La Bonita Taqueria", "Smokehouse Tavern", "Pembiche", "Kay's Bar", "Gnarly Grey", "Slappy Cakes", "Mi Mero Mole"};
+    private String[] cuisines = new String[] {"Vegan Food", "Breakfast", "Fishs Dishs", "Scandinavian", "Coffee", "English Food", "Burgers", "Fast Food", "Noodle Soups", "Mexican", "BBQ", "Cuban", "Bar Food", "Sports Bar", "Breakfast", "Mexican" };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
