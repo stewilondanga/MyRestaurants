72a20fb9d0003448807e2c88df9af00372396088
Stewart ilondanga <stilondanga@gmail.com>
2018-06-05 12:33:12 +0300

    add app/src/main/java/io/github/stewilondanga/myrestaurants/RestaurantsActivity.java

---
+++ app/src/main/java/io/github/stewilondanga/myrestaurants/RestaurantsActivity.java
@@ -0,0 +1,16 @@
+package io.github.stewilondanga.myrestaurants;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RestaurantsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_restaurants);
+        Intent intent = getIntent();
+        String location = intent.getStringExtra("location");
+    }
+}
