eb7bf5289e2ae6b3a1ae2886b0296efecac35908
Stewart ilondanga <stilondanga@gmail.com>
2018-06-05 11:20:45 +0300

    add app/src/main/java/io/github/stewilondanga.myrestaurants/MainActivity.java

--- app/src/main/java/io/github/stewilondanga/myrestaurants/MainActivity.java
+++ app/src/main/java/io/github/stewilondanga/myrestaurants/MainActivity.java
@@ -2,12 +2,16 @@ package io.github.stewilondanga.myrestaurants;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private Button mFindRestaurantsButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mFindRestaurantsButton = (Button) findViewById(R.id.findRestaurantsButton);
     }
 }
