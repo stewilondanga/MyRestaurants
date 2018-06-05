a239adf7b4a7ccc16237753261ae368f20a6b353
Stewart ilondanga <stilondanga@gmail.com>
2018-06-05 11:27:58 +0300

    add app/src/main/java/io/github/stewilondanga/myrestaurants/MainActivity.java

--- app/src/main/java/io/github/stewilondanga/myrestaurants/MainActivity.java
+++ app/src/main/java/io/github/stewilondanga/myrestaurants/MainActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     private Button mFindRestaurantsButton;
@@ -17,7 +18,7 @@ public class MainActivity extends AppCompatActivity {
                mFindRestaurantsButton.setOnClickListener(new View.OnClickListener() {
                    @Override
                    public void onClick(View v) {
-                       //do something
+                       Toast.makeText(MainActivity.this, "Hello World!", Toast.LENGTH_LONG).show();
                    }
                });
     }
