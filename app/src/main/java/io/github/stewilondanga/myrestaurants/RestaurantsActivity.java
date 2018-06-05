d030755890ee27fcfc84dac0e74342beba60a5f3
Stewart ilondanga <stilondanga@gmail.com>
2018-06-05 15:07:20 +0300

    add app/src/main/java/io/github/stewilondanga/myrestaurants/RestaurantsActivity.java

--- app/src/main/java/io/github/stewilondanga/myrestaurants/RestaurantsActivity.java
+++ app/src/main/java/io/github/stewilondanga/myrestaurants/RestaurantsActivity.java
@@ -4,16 +4,22 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
+import android.widget.ListView;
 
 public class RestaurantsActivity extends AppCompatActivity {
-    private String[] restaurants = new String[] {"Mi Mero Mole", "Mother's Bistro", "Life of Pie", "Screen Door", "Luc Lac", "Sweet Basil", "Slappy Cakes", "Equinox", "Miss Delta's", "Andina", "Lardo", "Portland City Grill", "Fat Head's Brewery", "Chipotle", "Subway"};
     private TextView mLocationTextView;
+    private ListView mListView;
+    private String[] restaurants = new String[] {"Mi Mero Mole", "Mother's Bistro", "Life of Pie", "Screen Door", "Luc Lac", "Sweet Basil", "Slappy Cakes", "Equinox", "Miss Delta's", "Andina", "Lardo", "Portland City Grill", "Fat Head's Brewery", "Chipotle", "Subway"};
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_restaurants);
+
+        mListView = (ListView) findViewById(R.id.listView);
         mLocationTextView = (TextView) findViewById(R.id.locationTextView);
+
         Intent intent = getIntent();
         String location = intent.getStringExtra("location");
         mLocationTextView.setText("Here are all the restaurants near: " + location);
