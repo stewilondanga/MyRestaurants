6714c59d0ed9424a6c34bd1b512641230aa40634
Stewart ilondanga <stilondanga@gmail.com>
2018-06-11 16:54:17 +0300

    add app/src/main/java/io/github/stewilondanga/myrestaurants/adapters/RestaurantListAdapter.java

--- app/src/main/java/io/github/stewilondanga/myrestaurants/adapters/RestaurantListAdapter.java
+++ app/src/main/java/io/github/stewilondanga/myrestaurants/adapters/RestaurantListAdapter.java
@@ -1,8 +1,20 @@
 package io.github.stewilondanga.myrestaurants.adapters;
 
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+
+import java.util.ArrayList;
+
 /**
  * Created by stewart on 6/11/18.
  */
 
-public class RestaurantListAdapter {
+public class RestaurantListAdapter extends RecyclerView.Adapter<RestaurantListAdapter.RestaurantViewHolder>{
+    private ArrayList<Restaurant>mRestaurants = new ArrayList<>();
+    private Context mContext;
+
+    public RestaurantListAdapter(Context context, ArrayList<Restaurant> restaurants) {
+        mContext = context;
+        mRestaurants = restaurants;
+    }
 }
