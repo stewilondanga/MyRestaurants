158facc8c63565d40302376c7bcb4a8a07d6b446
Stewart ilondanga <stilondanga@gmail.com>
2018-06-11 17:16:53 +0300

    add app/src/main/java/io/github/stewilondanga/myrestaurants/adapters/RestaurantListAdapter.java

--- app/src/main/java/io/github/stewilondanga/myrestaurants/adapters/RestaurantListAdapter.java
+++ app/src/main/java/io/github/stewilondanga/myrestaurants/adapters/RestaurantListAdapter.java
@@ -2,8 +2,15 @@ package io.github.stewilondanga.myrestaurants.adapters;
 
 import android.content.Context;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import java.util.ArrayList;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import io.github.stewilondanga.myrestaurants.R;
 
 /**
  * Created by stewart on 6/11/18.
@@ -16,5 +23,26 @@ public class RestaurantListAdapter extends RecyclerView.Adapter<RestaurantListAd
     public RestaurantListAdapter(Context context, ArrayList<Restaurant> restaurants) {
         mContext = context;
         mRestaurants = restaurants;
+    }
+
+    public class RestaurantViewHolder extends RecyclerView.ViewHolder {
+        @BindView(R.id.restaurantImageView) ImageView mRestaurantImageView;
+        @BindView(R.id.restaurantNameTextView) TextView mNameTextView;
+        @BindView(R.id.categoryTextView) TextView mCategoryTextView;
+        @BindView(R.id.ratingTextView) TextView mRatingTextView;
+
+        private Context mContext;
+
+        public RestaurantViewHolder(View itemView) {
+            super(itemView);
+            ButterKnife.bind(this, itemView);
+            mContext = itemView.getContext();
+        }
+
+        public void bindRestaurant(Restaurant restaurant) {
+            mNameTextView.setText(restaurant.getName());
+            mCategoryTextView.setText(restaurant.getCategories().get(0));
+            mRatingTextView.setText("Rating: " + restaurant.getRating() + "/5");
+        }
     }
 }
