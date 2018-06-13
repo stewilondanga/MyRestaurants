97295210560c8e407d2a3611542b5b2f61bdd32e
Stewart ilondanga <stilondanga@gmail.com>
2018-06-13 09:45:31 +0300

    add app/src/main/java/io/github/stewilondanga/myrestaurants/ui/RestaurantDetailActivity.java

--- app/src/main/java/io/github/stewilondanga/myrestaurants/ui/RestaurantDetailActivity.java
+++ app/src/main/java/io/github/stewilondanga/myrestaurants/ui/RestaurantDetailActivity.java
@@ -3,11 +3,35 @@ package io.github.stewilondanga.myrestaurants.ui;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.io.IOException;
+
+import io.github.stewilondanga.myrestaurants.R;
+import io.github.stewilondanga.myrestaurants.services.YelpService;
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.Response;
+
 public class RestaurantDetailActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_restaurant_detail);
+    }
+
+
+    private void getRestaurants(String location) {
+        final YelpService yelpService = new YelpService();
+        yelpService.findRestaurants(location, new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+
+            }
+        });
     }
 }
