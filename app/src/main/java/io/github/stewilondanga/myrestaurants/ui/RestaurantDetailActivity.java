e486b9bacaf80fa9d6a4e91df1cbb3880dd9ca41
Stewart ilondanga <stilondanga@gmail.com>
2018-06-20 13:03:48 +0300

    add app/src/main/java/io/github/stewilondanga/myrestaurants/ui/RestaurantDetailActivity.java

--- app/src/main/java/io/github/stewilondanga/myrestaurants/ui/RestaurantDetailActivity.java
+++ app/src/main/java/io/github/stewilondanga/myrestaurants/ui/RestaurantDetailActivity.java
@@ -1,22 +1,48 @@
 package io.github.stewilondanga.myrestaurants.ui;
 
-import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 
 import java.io.IOException;
 
-import butterknife.ButterKnife;
 import io.github.stewilondanga.myrestaurants.R;
+import io.github.stewilondanga.myrestaurants.adapters.RestaurantPagerAdapter;
+import io.github.stewilondanga.myrestaurants.models.Restaurant;
 import io.github.stewilondanga.myrestaurants.adapters.RestaurantListAdapter;
 import io.github.stewilondanga.myrestaurants.services.YelpService;
 import okhttp3.Call;
 import okhttp3.Callback;
 import okhttp3.Response;
 
-public class RestaurantDetailActivity extends AppCompatActivity {
+import org.parceler.Parcels;
 
+import java.util.ArrayList;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class RestaurantDetailActivity extends AppCompatActivity {
+    @BindView(R.id.viewPager) ViewPager mViewPager;
+    private RestaurantPagerAdapter adapterViewPager;
+    ArrayList<Restaurant> mRestaurants = new ArrayList<>();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_restaurant_detail);
+        ButterKnife.bind(this);
+
+        mRestaurants = Parcels.unwrap(getIntent().getParcelableExtra("restaurants"));
+        int startingPosition = getIntent().getIntExtra("position", 0);
+
+        adapterViewPager = new RestaurantPagerAdapter(getSupportFragmentManager(), mRestaurants);
+        mViewPager.setAdapter(adapterViewPager);
+        mViewPager.setCurrentItem(startingPosition);
     }
+
+}
