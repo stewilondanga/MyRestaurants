e4e2f9e85688dafb3f080b04bb18bc5e4752868a
Stewart ilondanga <stilondanga@gmail.com>
2018-06-21 16:18:19 +0300

    add app/src/main/java/io/github/stewilondanga/myrestaurants/Constants.java

--- app/src/main/java/io/github/stewilondanga/myrestaurants/Constants.java
+++ app/src/main/java/io/github/stewilondanga/myrestaurants/Constants.java
@@ -8,5 +8,6 @@ public class Constants {
     public static final String API_KEY = BuildConfig.API_KEY;
     public static final String YELP_BASE_URL = "https://api.yelp.com/v3/businesses/search";
     public static final String YELP_LOCATION_QUERY = "location";
-    public static final String PREFERENCES_LOCATION_KEY = "location";
+    //public static final String PREFERENCES_LOCATION_KEY = "location";
+    public static final String FIREBASE_CHILD_SEARCHED_LOCATION = "searchedLocation";
 }
