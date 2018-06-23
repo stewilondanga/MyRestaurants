6376486ae5ec50973e17fb602b32b59ab8219163
Stewart ilondanga <stilondanga@gmail.com>
2018-06-23 19:44:41 +0300

    add app/src/main/res/layout/activity_create_account.xml

---
+++ app/src/main/java/io/github/stewilondanga/myrestaurants/ui/CreateAccountActivity.java
@@ -0,0 +1,15 @@
+package io.github.stewilondanga.myrestaurants.ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import io.github.stewilondanga.myrestaurants.R;
+
+public class CreateAccountActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_account);
+    }
+}
