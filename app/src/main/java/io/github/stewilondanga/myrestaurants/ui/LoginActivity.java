840af1006d4959bfbdfba5962ccf73de87be6c57
Stewart ilondanga <stilondanga@gmail.com>
2018-06-25 12:51:21 +0300

    add app/src/main/java/io/github/stewilondanga/myrestaurants/ui/LoginActivity.java

--- app/src/main/java/io/github/stewilondanga/myrestaurants/ui/LoginActivity.java
+++ app/src/main/java/io/github/stewilondanga/myrestaurants/ui/LoginActivity.java
@@ -1,15 +1,33 @@
 package io.github.stewilondanga.myrestaurants.ui;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
 import io.github.stewilondanga.myrestaurants.R;
 
 public class LoginActivity extends AppCompatActivity {
+    @BindView(R.id.registerTextView) TextView mRegisterTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        ButterKnife.bind(this);
+        mRegisterTextView.setOnContextClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (view == mRegisterTextView){
+            Intent intent = new Intent(LoginActivity.this, CreateAccountActivity.class);
+            startActivity(intent);
+            finish();
+        }
     }
 }
