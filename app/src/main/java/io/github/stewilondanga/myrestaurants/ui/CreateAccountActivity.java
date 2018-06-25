acbbab901fab8fc8c1ab15b66b3a1486ff1f1f3b
Stewart ilondanga <stilondanga@gmail.com>
2018-06-25 18:27:34 +0300

    add app/src/main/java/io/github/stewilondanga/myrestaurants/ui/CreateAccountActivity.java

--- app/src/main/java/io/github/stewilondanga/myrestaurants/ui/CreateAccountActivity.java
+++ app/src/main/java/io/github/stewilondanga/myrestaurants/ui/CreateAccountActivity.java
@@ -5,6 +5,7 @@ import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -106,4 +107,33 @@ public class CreateAccountActivity extends AppCompatActivity {
             }
         };
     }
+
+    private boolean isValidEmail(String email) {
+        boolean isGoodEmail = (email != null && Patterns.EMAIL_ADDRESS.matcher(email).matches());
+        if (!isGoodEmail) {
+            mEmailEditText.setError("Please enter a valid email address");
+            return false;
+        }
+        return isGoodEmail;
+    }
+
+    private boolean isValidName(String name) {
+        if (name.equals("")){
+            mNameEditText.setError("Please enter your name");
+            return false;
+        }
+        return true;
+    }
+
+    private boolean isValidPassword(String password, String confirmPasswrd){
+        if (password.length() <6){
+            mPasswordEditText.setError("Please create a password containing at least 6 characters");
+            return false;
+        } else if (!password.equals(confirmPasswrd)){
+            mPasswordEditText.setError("Passwords do not match");
+            return false;
+        }
+        return true;
+    }
+
 }
