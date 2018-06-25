7b657921bf5be5fb11e267641f53a4c22de6d65b
Stewart ilondanga <stilondanga@gmail.com>
2018-06-25 10:01:47 +0300

    add app/src/main/java/io/github/stewilondanga/myrestaurants/ui/MainActivity.java

--- app/src/main/java/io/github/stewilondanga/myrestaurants/ui/MainActivity.java
+++ app/src/main/java/io/github/stewilondanga/myrestaurants/ui/MainActivity.java
@@ -25,43 +25,17 @@ import io.github.stewilondanga.myrestaurants.R;
 
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
-    // private SharedPreferences mSharedPreferences;
-    // private SharedPreferences.Editor mEditor;
-
-    private DatabaseReference mSearchedLocationReference;
-
-    private ValueEventListener mSearchedLocationReferenceListener;
-
-    @BindView(R.id.findRestaurantsButton) Button mFindRestaurantsButton;
-    @BindView(R.id.locationEditText) EditText mLocationEditText;
-    @BindView(R.id.textView) TextView mAppNameTextView;
-    @BindView(R.id.savedRestaurantsButton) Button mSavedRestaurantsButton;
+    @BindView(R.id.findRestaurantsButton)
+    Button mFindRestaurantsButton;
+    @BindView(R.id.locationEditText)
+    EditText mLocationEditText;
+    @BindView(R.id.textView)
+    TextView mAppNameTextView;
+    @BindView(R.id.savedRestaurantsButton)
+    Button mSavedRestaurantsButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
-        mSearchedLocationReference = FirebaseDatabase
-                .getInstance()
-                .getReference()
-                .child(Constants.FIREBASE_CHILD_SEARCHED_LOCATION);
-
-        mSavedRestaurantsButton.setOnClickListener(this);
-
-        mSearchedLocationReferenceListener = mSearchedLocationReference.addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(DataSnapshot dataSnapshot) {
-                for (DataSnapshot locationSnapshot : dataSnapshot.getChildren()) {
-                    String location = locationSnapshot.getValue().toString();
-                    Log.d("Locations updated", "location: " + location);
-                }
-            }
-
-            @Override
-            public void onCancelled(DatabaseError databaseError) {
-
-            }
-        });
-
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
@@ -69,46 +43,22 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         Typeface ostrichFont = Typeface.createFromAsset(getAssets(), "fonts/ostrich-regular.ttf");
         mAppNameTextView.setTypeface(ostrichFont);
 
-        // mSharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
-        // mEditor = mSharedPreferences.edit();
-
         mFindRestaurantsButton.setOnClickListener(this);
+        mSavedRestaurantsButton.setOnClickListener(this);
 
     }
 
     @Override
     public void onClick(View v) {
-        if (v == mFindRestaurantsButton) {
-            String location = mLocationEditText.getText().toString();
 
-            saveLocationToFirebase(location);
+        if (v == mFindRestaurantsButton) {
+            Intent intent = new Intent(MainActivity.this, RestaurantsListActivity.class);
+            StartActivity(intent);
+        }
 
         if (v == mSavedRestaurantsButton) {
             Intent intent = new Intent(MainActivity.this, SavedRestaurantListActivity.class);
             startActivity(intent);
         }
-
-            // if(!(location).equals("")){
-            //    addToSharedPreferences(location);
-            //}
-
-            Intent intent = new Intent(MainActivity.this, RestaurantsListActivity.class);
-            intent.putExtra("location", location);
-            startActivity(intent);
-        }
     }
-
-    public void saveLocationToFirebase(String location) {
-        mSearchedLocationReference.push().setValue(location);
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        mSearchedLocationReference.removeEventListener(mSearchedLocationReferenceListener);
-    }
-
-    // private void addToSharedPreferences(String location) {
-    //    mEditor.putString(Constants.PREFERENCES_LOCATION_KEY, location).apply();
-    //}
 }
