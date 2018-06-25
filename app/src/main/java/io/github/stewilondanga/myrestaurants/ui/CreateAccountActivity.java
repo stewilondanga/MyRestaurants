d50bc1484de775e01f6be05e4f2cb2e64def9569
Stewart ilondanga <stilondanga@gmail.com>
2018-06-25 14:08:09 +0300

    add app/src/main/java/io/github/stewilondanga/myrestaurants/ui/CreateAccountActivity.java

--- app/src/main/java/io/github/stewilondanga/myrestaurants/ui/CreateAccountActivity.java
+++ app/src/main/java/io/github/stewilondanga/myrestaurants/ui/CreateAccountActivity.java
@@ -15,6 +15,7 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -29,6 +30,7 @@ public class CreateAccountActivity extends AppCompatActivity {
     @BindView(R.id.confirmPasswordEditText) EditText mConfirmPasswordEditText;
     @BindView(R.id.loginTextView) TextView mLoginTextView;
     private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,6 +41,7 @@ public class CreateAccountActivity extends AppCompatActivity {
         mLoginTextView.setOnClickListener(this);
         mCreateUserButton.setOnClickListener(this);
         mAuth = FirebaseAuth.getInstance();
+        createAuthStateListener();
     }
 
     @Override
@@ -73,5 +76,20 @@ public class CreateAccountActivity extends AppCompatActivity {
                         }
                     }
                 });
+    }
+
+    private void createAuthStateListener(){
+        mAuthListener = new FirebaseAuth.AuthStateListener(){
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth){
+                final FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    Intent intent = new Intent(CreateAccountActivity.this, MainActivity.class);
+                    intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                    startActivity(intent);
+                    finish();
+                }
+            }
+        };
     }
 }
