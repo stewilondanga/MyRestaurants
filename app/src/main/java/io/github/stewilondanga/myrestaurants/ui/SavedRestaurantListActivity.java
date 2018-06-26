2e61f955fbc56d5dcd8080ad1560827ec131aad8
Stewart ilondanga <stilondanga@gmail.com>
2018-06-26 14:48:45 +0300

    add app/src/main/java/io/github/stewilondanga/myrestaurants/ui/SavedRestaurantListActivity.java

--- app/src/main/java/io/github/stewilondanga/myrestaurants/ui/SavedRestaurantListActivity.java
+++ app/src/main/java/io/github/stewilondanga/myrestaurants/ui/SavedRestaurantListActivity.java
@@ -1,22 +1,22 @@
 package io.github.stewilondanga.myrestaurants.ui;
 
-import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.view.ViewGroup;
 
+import io.github.stewilondanga.myrestaurants.Constants;
+import io.github.stewilondanga.myrestaurants.R;
+import io.github.stewilondanga.myrestaurants.adapters.FirebaseRestaurantViewHolder;
+import io.github.stewilondanga.myrestaurants.models.Restaurant;
 import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
-import io.github.stewilondanga.myrestaurants.Constants;
-import io.github.stewilondanga.myrestaurants.R;
-import io.github.stewilondanga.myrestaurants.adapters.FirebaseRestaurantViewHolder;
-import io.github.stewilondanga.myrestaurants.models.Restaurant;
 
 public class SavedRestaurantListActivity extends AppCompatActivity {
     private DatabaseReference mRestaurantReference;
@@ -31,7 +31,14 @@ public class SavedRestaurantListActivity extends AppCompatActivity {
         setContentView(R.layout.activity_restaurants);
         ButterKnife.bind(this);
 
-        mRestaurantReference = FirebaseDatabase.getInstance().getReference(Constants.FIREBASE_CHILD_RESTAURANTS);
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        String uid = user.getUid();
+
+        mRestaurantReference = FirebaseDatabase
+                .getInstance()
+                .getReference(Constants.FIREBASE_CHILD_RESTAURANTS);
+                .child(uid);
+
         setUpFirebaseAdapter();
     }
 
