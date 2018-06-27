5ec4fb52c8d9d29e1d3708ed8c7f511f4b81c2c4
Stewart ilondanga <stilondanga@gmail.com>
2018-06-27 12:27:04 +0300

    add app/src/main/java/io/github/stewilondanga/myrestaurants/adapters/FirebaseRestaurantListAdapter.java

---
+++ app/src/main/java/io/github/stewilondanga/myrestaurants/adapters/FirebaseRestaurantListAdapter.java
@@ -0,0 +1,57 @@
+package io.github.stewilondanga.myrestaurants.adapters;
+
+import android.content.Context;
+import android.support.v4.view.MotionEventCompat;
+import android.view.MotionEvent;
+import android.view.View;
+
+import io.github.stewilondanga.myrestaurants.adapters.FirebaseRestaurantViewHolder;
+import io.github.stewilondanga.myrestaurants.models.Restaurant;
+import io.github.stewilondanga.myrestaurants.util.ItemTouchHelperAdapter;
+import io.github.stewilondanga.myrestaurants.util.OnStartDragListener;
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.Query;
+
+/**
+ * Created by stewart on 6/27/18.
+ */
+
+public class FirebaseRestaurantListAdapter extends FirebaseRecyclerAdapter<Restaurant, FirebaseRestaurantViewHolder> implements
+    private DatabaseReference mRef;
+    private OnStartDragListener mOnStartDragListener;
+    private Context mContext;
+
+    public FirebaseRestaurantListAdapter(Class<Restaurant> modelClass, int modelLayout, Class<FirebaseRestaurantViewHolder> viewHolderClass, Query ref, OnStartDragListener onStartDragListener, Context context) {
+        super(modelClass, modelLayout, viewHolderClass, ref);
+        mRef = ref.getRef();
+        mOnStartDragListener = onStartDragListener;
+        mContext = context;
+    }
+
+    @Override
+    protected void populateViewHolder(final FirebaseRestaurantViewHolder viewHolder, Restaurant model, int position){
+        viewHolder.bindRestaurant(model);
+
+        viewHolder.mRestaurantImageView.setOnTouchListener(new View.OnTouchListener()){
+
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                if (MotionEventCompat.getActionMasked(event) == MotionEvent.ACTION_DOWN){
+                    mOnStartDragListener.onStartDrag(viewHolder);
+                }
+                return false;
+        }
+    });
+}
+
+@Override
+public boolean onItemMove(int fromPosition, int toPosition){
+    return false;
+}
+
+@Override
+public void onItemDismiss(int position){
+
+}
+}
