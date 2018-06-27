50695b344b9734aa5866b8ae4fe5e9470b65850e
Stewart ilondanga <stilondanga@gmail.com>
2018-06-27 10:20:46 +0300

    add app/src/main/java/io/github/stewilondanga/myrestaurants/util/ItemTouchHelperAdapter.java

---
+++ app/src/main/java/io/github/stewilondanga/myrestaurants/util/ItemTouchHelperAdapter.java
@@ -0,0 +1,10 @@
+package io.github.stewilondanga.myrestaurants.util;
+
+/**
+ * Created by stewart on 6/27/18.
+ */
+
+public class ItemTouchHelperAdapter {
+    boolean onItemMove(int fromPosition, int toPosition);
+    void onItemDismiss(int position);
+}
