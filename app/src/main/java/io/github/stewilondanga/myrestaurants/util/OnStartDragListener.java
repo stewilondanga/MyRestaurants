33d2a5c35f36969ad371f6f6c636eb2724fbffb8
Stewart ilondanga <stilondanga@gmail.com>
2018-06-27 10:25:58 +0300

    add app/src/main/java/io/github/stewilondanga/myrestaurants/util/OnStartDragListener.java

---
+++ app/src/main/java/io/github/stewilondanga/myrestaurants/util/OnStartDragListener.java
@@ -0,0 +1,11 @@
+package io.github.stewilondanga.myrestaurants.util;
+
+import android.support.v7.widget.RecyclerView;
+
+/**
+ * Created by stewart on 6/27/18.
+ */
+
+public class OnStartDragListener {
+    void onStartDrag(RecyclerView.ViewHolder viewHolder);
+}
