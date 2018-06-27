b89d486a8548a1d027e377a33c21c9c952dba6c4
Stewart ilondanga <stilondanga@gmail.com>
2018-06-27 16:02:33 +0300

    add app/src/main/java/io/github/stewilondanga/myrestaurants/util/ItemTouchHelperViewHolder.java

---
+++ app/src/main/java/io/github/stewilondanga/myrestaurants/util/ItemTouchHelperViewHolder.java
@@ -0,0 +1,10 @@
+package io.github.stewilondanga.myrestaurants.util;
+
+/**
+ * Created by stewart on 6/27/18.
+ */
+
+public class ItemTouchHelperViewHolder {
+    void onItemSelected();
+    void onItemClear();
+}
