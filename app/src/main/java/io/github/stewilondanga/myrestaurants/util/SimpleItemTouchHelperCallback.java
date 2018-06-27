ebd7358bbf5a7e387b1d63938a41bb3a1ea38c58
Stewart ilondanga <stilondanga@gmail.com>
2018-06-27 10:44:20 +0300

    add app/src/main/java/io/github/stewilondanga/myrestaurants/util/SimpleItemTouchHelperCallback.java

---
+++ app/src/main/java/io/github/stewilondanga/myrestaurants/util/SimpleItemTouchHelperCallback.java
@@ -0,0 +1,47 @@
+package io.github.stewilondanga.myrestaurants.util;
+
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.helper.ItemTouchHelper;
+
+/**
+ * Created by stewart on 6/27/18.
+ */
+
+public class SimpleItemTouchHelperCallback extends ItemTouchHelper.Callback {
+    private final ItemTouchHelperAdapter mAdapter;
+
+    public SimpleItemTouchHelperCallback(ItemTouchHelperAdapter adapter){
+        mAdapter = adapter;
+    }
+
+    @Override
+    public boolean isLongPressDragEnabled(){
+        return true;
+    }
+
+    @Override
+    public boolean isItemViewSwipeEnabled() {
+        return true;
+    }
+
+    @Override
+    public int getMovementFlags(RecyclerView recyclerView, RecyclerView.ViewHolder viewHolder){
+        final int dragFlags = ItemTouchHelper.UP | ItemTouchHelper.DOWN;
+        final int swipeFlags = ItemTouchHelper.START | ItemTouchHelper.END;
+        return makeMovementFlags(dragFlags, swipeFlags);
+    }
+
+    @Override
+    public boolean onMove(RecyclerView recyclerView, RecyclerView.ViewHolder source, RecyclerView.ViewHolder target){
+        if (source.getItemViewType() != target.getItemViewType()){
+            return false;
+        }
+        mAdapter.onItemMove(source.getAdapterPosition(), target.getAdapterPosition());
+        return true;
+    }
+
+    @Override
+    public void onSwiped(RecyclerView.ViewHolder viewHolder, int i){
+        mAdapter.onItemDismiss(viewHolder.getAdapterPosition());
+    }
+}
