87b41a6bc3a104208c614c8275bc68c67c359483
Stewart ilondanga <stilondanga@gmail.com>
2018-06-27 19:23:06 +0300

    add app/src/main/res/layout/fragment_restaurant_list.xml

---
+++ app/src/main/java/io/github/stewilondanga/myrestaurants/ui/RestaurantListFragment.java
@@ -0,0 +1,132 @@
+package io.github.stewilondanga.myrestaurants.ui;
+
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.support.v4.app.Fragment;
+import android.support.v4.view.MenuItemCompat;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.SearchView;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+
+import io.github.stewilondanga.myrestaurants.Constants;
+import io.github.stewilondanga.myrestaurants.R;
+import io.github.stewilondanga.myrestaurants.adapters.RestaurantListAdapter;
+import io.github.stewilondanga.myrestaurants.models.Restaurant;
+import io.github.stewilondanga.myrestaurants.services.YelpService;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.Response;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class RestaurantListFragment extends Fragment {
+    @BindView(R.id.recyclerView) RecyclerView mRecyclerView;
+
+    private RestaurantListAdapter mAdapter;
+    public ArrayList<Restaurant> mRestaurants = new ArrayList<>();
+    private SharedPreferences mSharedPreferences;
+    private SharedPreferences.Editor mEditor;
+    private String mRecentAddress;
+
+    public RestaurantListFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        mSharedPreferences = PreferenceManager.getDefaultSharedPreferences(getActivity());
+        mEditor = mSharedPreferences.edit();
+
+        setHasOptionsMenu(true);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState){
+        View view = inflater.inflate(R.layout.fragment_restaurant_list, container, false);
+        ButterKnife.bind(this, view);
+        mRecentAddress = mSharedPreferences.getString(Constants.PREFERENCES_LOCATION_KEY, null);
+
+        if (mRecentAddress !=null){
+            getRestaurants(mRecentAddress);
+        }
+
+        return view;
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater){
+        super.onCreateOptionsMenu(menu, inflater);
+        inflater.inflate(R.menu.menu_search, menu);
+
+        MenuItem menuItem = menu.findItem(R.id.action_search);
+        SearchView searchView = (SearchView) MenuItemCompat.getActionView(menuItem);
+
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                addToSharedPreferences(query);
+                getRestaurants(query);
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                return false;
+            }
+        });
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void getRestaurants(String location){
+       final YelpService yelpService = new YelpService();
+
+       yelpService.findRestaurants(location, new Callback() {
+           @Override
+           public void onFailure(Call call, IOException e) {
+               e.printStackTrace();
+           }
+
+           @Override
+           public void onResponse(Call call, Response response) throws IOException {
+                mRestaurants = yelpService.processResults(response);
+
+                getActivity().runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        mAdapter = new RestaurantListAdapter(getActivity(), mRestaurants);
+                        mRecyclerView.setAdapter(mAdapter);
+                        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getActivity());
+                        mRecyclerView.setLayoutManager(layoutManager);
+                        mRecyclerView.setHasFixedSize(true);
+                    }
+                });
+           }
+       });
+    }
+
+    private void addToSharedPreferences(String location) {
+        mEditor.putString(Constants.YELP_LOCATION_QUERY, location).apply();
+    }
+
+}
