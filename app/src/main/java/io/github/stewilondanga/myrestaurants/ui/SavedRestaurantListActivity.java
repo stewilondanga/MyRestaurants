d46fe1bfc6466409a5360634531793fe227a2633
Stewart ilondanga <stilondanga@gmail.com>
2018-06-27 19:40:30 +0300

    add app/src/main/java/io/github/stewilondanga/myrestaurants/ui/SavedRestaurantListActivity.java

--- app/src/main/java/io/github/stewilondanga/myrestaurants/ui/SavedRestaurantListActivity.java
+++ app/src/main/java/io/github/stewilondanga/myrestaurants/ui/SavedRestaurantListActivity.java
@@ -2,71 +2,15 @@ package io.github.stewilondanga.myrestaurants.ui;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
-import android.support.v7.widget.helper.ItemTouchHelper;
 
-import io.github.stewilondanga.myrestaurants.Constants;
 import io.github.stewilondanga.myrestaurants.R;
-import io.github.stewilondanga.myrestaurants.adapters.FirebaseRestaurantViewHolder;
-import io.github.stewilondanga.myrestaurants.models.Restaurant;
-import io.github.stewilondanga.myrestaurants.adapters.FirebaseRestaurantListAdapter;
 import io.github.stewilondanga.myrestaurants.util.OnStartDragListener;
-import io.github.stewilondanga.myrestaurants.util.SimpleItemTouchHelperCallback;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.Query;
 
-import butterknife.BindView;
-import butterknife.ButterKnife;
 
 public class SavedRestaurantListActivity extends AppCompatActivity implements OnStartDragListener {
-    private FirebaseRestaurantListAdapter mFirebaseAdapter;
-    private ItemTouchHelper itemTouchHelper;
-
-    @BindView(R.id.recyclerView) RecyclerView mRecyclerView;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_restaurants);
-        ButterKnife.bind(this);
-
-        setUpFirebaseAdapter();
-    }
-
-    private void setUpFirebaseAdapter(){
-        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
-        String uid = user.getUid();
-
-        Query = FirebaseDatabase
-                .getInstance()
-                .getReference(Constants.FIREBASE_CHILD_RESTAURANTS);
-                .child(uid);
-                .orderByChild(Constants.FIREBASE_QUERY_INDEX);
-
-        mFirebaseAdapter = new FirebaseRestaurantListAdapter(Restaurant.class, R.layout.restaurant_list_item_drag, FirebaseRestaurantViewHolder.class, query,this, this);
-        mRecyclerView.setHasFixedSize(true);
-        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
-        mRecyclerView.setAdapter(mFirebaseAdapter);
-
-        ItemTouchHelper.Callback callback = new SimpleItemTouchHelperCallback(mFirebaseAdapter);
-        mItemTouchHelper = new ItemTouchHelper(callback);
-        mItemTouchHelper.attachToRecyclerView(mRecyclerView);
-    }
-
-    @Override
-    public void onStartDrag(RecyclerView.ViewHolder viewHolder){
-        mItemTouchHelper.startDrag(viewHolder);
-    }
-
-    @Override
-    protected void onDestroy(){
-        super.onDestroy();
-        mFirebaseAdapter.cleanup();
     }
 }
-
-
