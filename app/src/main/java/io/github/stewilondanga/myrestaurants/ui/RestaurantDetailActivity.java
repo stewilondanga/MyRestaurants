fdd55e90ffda51a39cbcb0a0d7b7b943ec14df43
Stewart ilondanga <stilondanga@gmail.com>
2018-06-27 15:40:58 +0300

    add app/src/main/java/io/github/stewilondanga/myrestaurants/ui/RestaurantDetailActivity.java

--- app/src/main/java/io/github/stewilondanga/myrestaurants/ui/RestaurantDetailActivity.java
+++ app/src/main/java/io/github/stewilondanga/myrestaurants/ui/RestaurantDetailActivity.java
@@ -1,23 +1,12 @@
 package io.github.stewilondanga.myrestaurants.ui;
 
-import android.os.Bundle;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
-import android.content.Intent;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
-import android.util.Log;
-
-import java.io.IOException;
+import android.os.Bundle;
 
 import io.github.stewilondanga.myrestaurants.R;
 import io.github.stewilondanga.myrestaurants.adapters.RestaurantPagerAdapter;
 import io.github.stewilondanga.myrestaurants.models.Restaurant;
-import io.github.stewilondanga.myrestaurants.adapters.RestaurantListAdapter;
-import io.github.stewilondanga.myrestaurants.services.YelpService;
-import okhttp3.Call;
-import okhttp3.Callback;
-import okhttp3.Response;
 
 import org.parceler.Parcels;
 
@@ -38,6 +27,7 @@ public class RestaurantDetailActivity extends AppCompatActivity {
         ButterKnife.bind(this);
 
         mRestaurants = Parcels.unwrap(getIntent().getParcelableExtra("restaurants"));
+
         int startingPosition = getIntent().getIntExtra("position", 0);
 
         adapterViewPager = new RestaurantPagerAdapter(getSupportFragmentManager(), mRestaurants);
