c5106df0ea1fa2947cde6e03b7360d27af021b94
Stewart ilondanga <stilondanga@gmail.com>
2018-06-30 09:00:34 +0300

    add app/src/main/java/io/github/stewilondanga/myrestaurants/util/OnRestaurantSelectedListener.java

---
+++ app/src/main/java/io/github/stewilondanga/myrestaurants/util/OnRestaurantSelectedListener.java
@@ -0,0 +1,13 @@
+package io.github.stewilondanga.myrestaurants.util;
+
+import io.github.stewilondanga.myrestaurants.models.Restaurant;
+
+import java.util.ArrayList;
+
+/**
+ * Created by stewart on 6/30/18.
+ */
+
+public interface OnRestaurantSelectedListener {
+    public void onRestaurantSelected(Integer position, ArrayList<Restaurant> restaurants);
+}
