9287e4a89bb78a0aaaed4bb5468be3f43bac8531
Stewart ilondanga <stilondanga@gmail.com>
2018-06-30 11:18:53 +0300

    add app/src/main/java/io/github/stewilondanga/myrestaurants/adapters/RestaurantListAdapter.java

--- app/src/main/java/io/github/stewilondanga/myrestaurants/adapters/RestaurantListAdapter.java
+++ app/src/main/java/io/github/stewilondanga/myrestaurants/adapters/RestaurantListAdapter.java
@@ -6,6 +6,7 @@ import android.content.res.Configuration;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -17,7 +18,6 @@ import io.github.stewilondanga.myrestaurants.R;
 import io.github.stewilondanga.myrestaurants.models.Restaurant;
 import io.github.stewilondanga.myrestaurants.ui.RestaurantDetailActivity;
 import io.github.stewilondanga.myrestaurants.ui.RestaurantDetailFragment;
-import io.github.stewilondanga.myrestaurants.util.OnRestaurantSelectedListener;
 import com.squareup.picasso.Picasso;
 
 import org.parceler.Parcels;
@@ -38,18 +38,16 @@ public class RestaurantListAdapter extends RecyclerView.Adapter<RestaurantListAd
 
     private List<Restaurant> mRestaurants = new ArrayList<>();
     private Context mContext;
-    private OnRestaurantSelectedListener mOnRestaurantSelectedListener;
 
-    public RestaurantListAdapter(Context context, List<Restaurant> restaurants, OnRestaurantSelectedListener restaurantSelectedListener) {
+    public RestaurantListAdapter(Context context, List<Restaurant> restaurants) {
         mContext = context;
         mRestaurants = restaurants;
-        mOnRestaurantSelectedListener = restaurantSelectedListener;
     }
 
     @Override
     public RestaurantListAdapter.RestaurantViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.restaurant_list_item, parent, false);
-        RestaurantViewHolder viewHolder = new RestaurantViewHolder(view, mRestaurants, mOnRestaurantSelectedListener);
+        RestaurantViewHolder viewHolder = new RestaurantViewHolder(view);
         return viewHolder;
     }
 
@@ -60,6 +58,7 @@ public class RestaurantListAdapter extends RecyclerView.Adapter<RestaurantListAd
 
     @Override
     public int getItemCount() {
+
         return mRestaurants.size();
     }
 
@@ -71,16 +70,12 @@ public class RestaurantListAdapter extends RecyclerView.Adapter<RestaurantListAd
 
         private Context mContext;
         private int mOrientation;
-        private List<Restaurant> mRestaurants = new ArrayList<>();
-        private OnRestaurantSelectedListener mRestaurantSelectedListener;
 
-        public RestaurantViewHolder(View itemView, List<Restaurant> restaurants, OnRestaurantSelectedListener restaurantSelectedListener) {
+        public RestaurantViewHolder(View itemView) {
             super(itemView);
             ButterKnife.bind(this, itemView);
 
             mContext = itemView.getContext();
-            mRestaurants = restaurants;
-            mRestaurantSelectedListener = restaurantSelectedListener;
 
             //Determines the current orientation of the device:
             mOrientation = itemView.getResources().getConfiguration().orientation;
@@ -108,7 +103,7 @@ public class RestaurantListAdapter extends RecyclerView.Adapter<RestaurantListAd
 
         private void createDetailFragment(int position) {
             // Creates new RestaurantDetailFragment with the given position:
-            RestaurantDetailFragment detailFragment = RestaurantDetailFragment.newInstance(mRestaurants, position);
+            RestaurantDetailFragment detailFragment = RestaurantDetailFragment.newInstance(mRestaurants, position, Constants.SOURCE_SAVED);
             // Gathers necessary components to replace the FrameLayout in the layout with the RestaurantDetailFragment:
             FragmentTransaction ft = ((FragmentActivity) mContext).getSupportFragmentManager().beginTransaction();
             // Replaces the FrameLayout with the RestaurantDetailFragment:
@@ -120,7 +115,6 @@ public class RestaurantListAdapter extends RecyclerView.Adapter<RestaurantListAd
         @Override
         public void onClick(View v) {
             int itemPosition = getLayoutPosition();
-            mRestaurantSelectedListener.onRestaurantSelected(itemPosition, mRestaurants);
 
             if (mOrientation == Configuration.ORIENTATION_LANDSCAPE) {
                 createDetailFragment(itemPosition);
